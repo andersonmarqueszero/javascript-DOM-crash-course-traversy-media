ba6fdd34687c15078633fcf144c6357805d9c688
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-01-22 21:22:08 -0300

    get elements by id + tag name

--- main.js
+++ main.js
@@ -26,3 +26,35 @@
 // console.log(headerTitle.innerText); //.innerText pays attention to the style
 // headerTitle.innerHTML = '<h3>Hello</h3>';
 // header.style.borderBottom = 'solid 3px #000';
+
+// Get Elements By Class Name //
+
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+// // GIVES ERROR
+// // items.style.backgroundColor = '#f4f4f4';
+
+// for(i = 0; i < items.length; i++){
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+//Get Elements By Tag Name
+
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+// // GIVES ERROR
+// // li.style.backgroundColor = '#f4f4f4';
+
+// for(i = 0; i < li.length; i++){
+//     li[i].style.backgroundColor = '#f4f4f4';
+// }
