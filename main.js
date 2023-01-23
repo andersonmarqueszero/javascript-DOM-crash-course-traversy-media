75eb2b099cf77d52dba2ea27160b089072081169
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-01-22 21:29:14 -0300

    query selector

--- main.js
+++ main.js
@@ -58,3 +58,23 @@
 // for(i = 0; i < li.length; i++){
 //     li[i].style.backgroundColor = '#f4f4f4';
 // }
+
+// Query Selector //
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND";
+
+// var item = document.querySelector('.list-group-tem');
+// item.style.color = 'red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'coral';
