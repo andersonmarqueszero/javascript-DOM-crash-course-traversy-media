ce84a3f54efc9c449dacc86d99a8ec31ba054aad
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-01-25 20:56:34 -0300

    events

--- main.js
+++ main.js
@@ -165,3 +165,33 @@
 // newDiv.style.fontSize = '30px';
 
 // container.insertBefore(newDiv, h1);
+
+// Events //
+
+var button = document.getElementById('button').addEventListener('click', buttonClick);
+
+function buttonClick(e){
+    // console.log('Button clicked');
+    // document.getElementById('header-title').textContent = 'Changed';
+    // document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+    //console.log(e);
+
+    // console.log(e.target);
+    // console.log(e.target.id);
+    // console.log(e.target.className);
+    // console.log(e.target.classList);
+    // var output = document.getElementById('output');
+    // output.innerHTML = '<h3>'+e.target.id+'</h3>'
+
+    // console.log(e.type);
+
+    // console.log(e.clientX);
+    // console.log(e.clientY);
+
+    // console.log(e.offsetX);
+    // console.log(e.offsetY);
+
+    // console.log(e.altKey);
+    // console.log(e.ctrlKey);
+    // console.log(e.shiftKey);
+}
